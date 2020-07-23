233d5f803654b623de09ff45df05527cbed596b9
wangjing1014 <wangjing1014@gmail.com>
2020-07-23 14:56:49 +0800

    Updates

---
+++ precache-manifest.7359fe6b96e3b2f2392111d6921cbaea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91e87af5d0bfc2a79f8e",
+    "url": "/react-test/static/js/runtime~main.b9ad4e01.js"
+  },
+  {
+    "revision": "ae9d20286a2589201e9c",
+    "url": "/react-test/static/js/main.376dc42e.chunk.js"
+  },
+  {
+    "revision": "f952c57abdda4a96d4b4",
+    "url": "/react-test/static/js/2.64fe35b5.chunk.js"
+  },
+  {
+    "revision": "ae9d20286a2589201e9c",
+    "url": "/react-test/static/css/main.ebcbaef2.chunk.css"
+  },
+  {
+    "revision": "b06b8b0d3180ae425151cb544718c673",
+    "url": "/react-test/index.html"
+  }
+];
